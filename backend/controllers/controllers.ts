857246b1a43857457f302ac9c83f083f36f80892
ngutu <gutunichita757@gmail.com>
2022-09-23 19:46:24 +0200

    logic to manage tasks

--- backend/controllers/controllers.ts
+++ backend/controllers/controllers.ts
@@ -1,7 +1,8 @@
-import bcrypt, { hash } from "bcrypt";
-import Joi, { string } from "joi";
+import bcrypt from "bcrypt";
+import Joi from "joi";
 import express from "express";
 import jwt from "jsonwebtoken";
+import { ObjectId } from "mongodb";
 
 import db from "../db/db.js"
 import { TOKEN_KEY } from "../config/config.js";
@@ -77,6 +78,12 @@ export async function register(req: express.Request, res: express.Response) {
 }
 
 export async function login(req: express.Request, res: express.Response) {
+    if (await authorized(getTokenFromHeaders(req))) {
+        return res.status(200).json({
+            ok: true
+        })
+    };
+
     const {username, password} = req.body;
     if (!await validCredentials(username, password)) {
         return res.status(400).json({
@@ -145,6 +152,164 @@ export async function logout(req: express.Request, res: express.Response) {
     })
 }
 
+export async function getAllTasks(req: express.Request, res: express.Response) {
+    const username = await authorized(getTokenFromHeaders(req));
+    if (!username) {
+        return res.status(401).json({
+            ok: false,
+            error: "unauthorized"
+        });
+    }
+
+    let tasks: Array<Object> = [];
+    try {
+        const cursor = db.tasksCollection.find({
+            owner: username
+        })
+
+        await cursor.forEach((task) => {
+            tasks.push(task);
+        })
+    } catch(err) {
+        return res.status(500).json({
+            ok: false,
+            error: "internal server error"
+        })
+    }
+
+    res.status(200).json({
+        ok: true,
+        tasks
+    })
+}
+
+export async function postTask(req: express.Request, res: express.Response) {
+    const username = await authorized(getTokenFromHeaders(req));
+    if (!username) {
+        return res.status(401).json({
+            ok: false,
+            error: "unauthorized"
+        });
+    }
+
+    const {description} = req.body;
+    if (!description) {
+        return res.status(400).json({
+            ok: false,
+            error: "invalid description"
+        });
+    }
+
+    let task;
+    try {
+        task = await db.tasksCollection.insertOne({
+            description: description,
+            status: false,
+            owner: username
+        })
+    } catch(err) {
+        return res.status(500).json({
+            ok: false,
+            error: "internal server error"
+        });
+    }
+
+    return res.status(201).json({
+        ok: true,
+        taskID: task.insertedId
+    })
+}
+
+export async function deleteTask(req: express.Request, res: express.Response) {
+    const username = await authorized(getTokenFromHeaders(req));
+    if (!username) {
+        return res.status(401).json({
+            ok: false,
+            error: "unauthorized"
+        })
+    }
+
+    if (!req.params['id']) {
+        return res.status(400).json({
+            ok: false,
+            error: "task ID wasn't provided"
+        })
+    }
+
+    let taskID;
+    try {
+        taskID = new ObjectId(req.params['id']);
+        await db.tasksCollection.deleteOne({
+            _id: taskID,
+            owner: username
+        })
+    } catch(err) {
+        return res.status(500).json({
+            ok: false,
+            error: "internal server error"
+        })
+    }
+
+    return res.status(200).json({
+        ok: true,
+        taskID: taskID
+    })
+}
+
+export async function patchTask(req: express.Request, res: express.Response) {
+    const username = await authorized(getTokenFromHeaders(req));
+    if (!username) {
+        return res.status(401).json({
+            ok: false,
+            error: "unauthorized"
+        })
+    }
+
+    if (!req.params['id']) {
+        return res.status(400).json({
+            ok: false,
+            error: "task ID wasn't provided"
+        })
+    }
+
+    let taskID;
+    try {
+        taskID = new ObjectId(req.params['id']);
+        const {description, status} = req.body;
+        if (description) {
+            await db.tasksCollection.updateOne({
+                _id: taskID,
+                owner: username
+            }, {
+                $set: {
+                    description: description
+                }
+            })
+        }
+
+        if (status || status === false) {
+            await db.tasksCollection.updateOne({
+                _id: taskID,
+                owner: username
+            }, { $set: {
+                    status: status
+                }
+            })
+        }
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({
+            ok: false,
+            error: "internal server error"
+        })
+    }
+
+    return res.status(200).json({
+        ok: true,
+        taskID: taskID
+    })
+}
+
 async function issueToken(username: string): Promise<string> {
     const token = jwt.sign({username: username}, TOKEN_KEY, {
         expiresIn: "1d",
@@ -180,7 +345,12 @@ async function authorized(token: string): Promise<string> {
         return ""
     }
 
-    if (!await redisClient.get(username)) {
+    const dbToken = await redisClient.get(username);
+    if (!dbToken) {
+        return ""
+    }
+
+    if (dbToken !== token) {
         return ""
     }
 
@@ -210,3 +380,10 @@ async function validCredentials(username: string, password: string): Promise<boo
 
     return true
 }
+
+export default {register, login, logout, getAllTasks, postTask, patchTask, deleteTask};
+
+//632df25533682d01cf861288
+//632df27033682d01cf861289
+//632df28333682d01cf86128a
+//632df28c33682d01cf86128b
--- backend/db/db.ts
+++ backend/db/db.ts
@@ -6,6 +6,12 @@ interface User {
     password: string;
 }
 
+interface Task {
+    description: string;
+    status: boolean;
+    owner: string;
+}
+
 const client = new MongoClient(MONGO_URI);
 try {
     await client.connect();
@@ -16,8 +22,11 @@ try {
 const db = client.db(MONGO_NAME);
 
 const usersCollection = db.collection<User>('users');
+const tasksCollection = db.collection<Task>('tasks');
 
 await usersCollection.createIndex({username: 1}, {unique: true});
+await tasksCollection.createIndex({description: 1}, {unique: true});
+await tasksCollection.createIndex({owner: 1}, {unique: false});
 
-export default {usersCollection}
+export default {usersCollection, tasksCollection};
 
